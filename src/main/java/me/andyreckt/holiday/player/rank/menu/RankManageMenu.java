eaa0981bf97382097cddcfe9c6bf012f92314f18
AndyReckt <andyreckt@gmail.com>
2022-01-15 15:14:54 +0100

    [+] Added a ConversationButton

--- src/main/java/me/andyreckt/holiday/player/rank/menu/RankManageMenu.java
+++ src/main/java/me/andyreckt/holiday/player/rank/menu/RankManageMenu.java
@@ -3,6 +3,7 @@ package me.andyreckt.holiday.player.rank.menu;
 import io.github.zowpy.menu.Button;
 import io.github.zowpy.menu.Menu;
 import io.github.zowpy.menu.buttons.BooleanButton;
+import io.github.zowpy.menu.buttons.ConversationButton;
 import io.github.zowpy.menu.buttons.DisplayButton;
 import io.github.zowpy.menu.buttons.Glass;
 import me.andyreckt.holiday.Holiday;
@@ -67,96 +68,33 @@ public class RankManageMenu extends Menu {
         toReturn.put(15, new BooleanButton<>(rank, "Admin", Rank::setAdmin, Rank::isAdmin, save));
         toReturn.put(16, new BooleanButton<>(rank, "Dev", Rank::setDev, Rank::isDev, save));
 
-        toReturn.put(21, new Button() {
-            @Override
-            public ItemStack getButtonItem(Player p0) {
-                return new ItemBuilder(Material.PAPER, "&dPrefix").lore(rank.getPrefix()).build();
-            }
-
-            @Override
-            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
-                player.closeInventory();
-                ConversationFactory factory = new ConversationFactory(Holiday.getInstance()).withModality(true).withPrefix(new NullConversationPrefix()).withFirstPrompt(new StringPrompt() {
-                    @Override
-                    public String getPromptText(ConversationContext cc) {
-                        return CC.translate("&aEnter the new prefix for this rank: &7&o(type \"CANCEL\" to cancel this)");
-                    }
-
-                    @Override
-                    public Prompt acceptInput(ConversationContext cc, String s) {
-                        if (s.equalsIgnoreCase("cancel")) {
-                            cc.getForWhom().sendRawMessage(ChatColor.RED + "Cancelled.");
-                            return Prompt.END_OF_CONVERSATION;
-                        }
-                        rank.setPrefix(s + " ");
-                        rank.save();
-                        cc.getForWhom().sendRawMessage(CC.translate("&aSuccessfully changed the prefix to "+ s ));
-                        return Prompt.END_OF_CONVERSATION;
-                    }
-                }).withLocalEcho(false).withEscapeSequence("/no").withTimeout(60).thatExcludesNonPlayersWithMessage("How did u get there???");
-                player.beginConversation(factory.buildConversation(player));
-            }
-        });
-        toReturn.put(22, new Button() {
-            @Override
-            public ItemStack getButtonItem(Player p0) {
-                return new ItemBuilder(Material.PAPER, "&dDisplay Name").lore(rank.getDisplayName() == null ? "&fNone" : rank.getDisplayName()).build();
-            }
-
-            @Override
-            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
-                player.closeInventory();
-                ConversationFactory factory = new ConversationFactory(Holiday.getInstance()).withModality(true).withPrefix(new NullConversationPrefix()).withFirstPrompt(new StringPrompt() {
-                    @Override
-                    public String getPromptText(ConversationContext cc) {
-                        return CC.translate("&aEnter the new display name for this rank: &7&o(type \"CANCEL\" to cancel this)");
-                    }
-
-                    @Override
-                    public Prompt acceptInput(ConversationContext cc, String s) {
-                        if (s.equalsIgnoreCase("cancel")) {
-                            cc.getForWhom().sendRawMessage(ChatColor.RED + "Cancelled.");
-                            return Prompt.END_OF_CONVERSATION;
-                        }
-                        rank.setDisplayName(s);
-                        rank.save();
-                        cc.getForWhom().sendRawMessage(CC.translate("&aSuccessfully changed the display name to "+ s ));
-                        return Prompt.END_OF_CONVERSATION;
-                    }
-                }).withLocalEcho(false).withEscapeSequence("/no").withTimeout(60).thatExcludesNonPlayersWithMessage("How did u get there???");
-                player.beginConversation(factory.buildConversation(player));
-            }
-        });
-        toReturn.put(23, new Button() {
-            @Override
-            public ItemStack getButtonItem(Player p0) {
-                return new ItemBuilder(Material.PAPER, "&dSuffix").lore(rank.getSuffix()).build();
-            }
-
-            @Override
-            public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
-                player.closeInventory();
-                ConversationFactory factory = new ConversationFactory(Holiday.getInstance()).withModality(true).withPrefix(new NullConversationPrefix()).withFirstPrompt(new StringPrompt() {
-                    @Override
-                    public String getPromptText(ConversationContext cc) {
-                        return CC.translate("&aEnter the new suffix for this rank: &7&o(type \"CANCEL\" to cancel this)");
-                    }
-
-                    @Override
-                    public Prompt acceptInput(ConversationContext cc, String s) {
-                        if (s.equalsIgnoreCase("cancel")) {
-                            cc.getForWhom().sendRawMessage(ChatColor.RED + "Cancelled.");
-                            return Prompt.END_OF_CONVERSATION;
-                        }
-                        rank.setSuffix(s);
-                        rank.save();
-                        cc.getForWhom().sendRawMessage(CC.translate("&aSuccessfully changed the suffix to "+ s ));
-                        return Prompt.END_OF_CONVERSATION;
-                    }
-                }).withLocalEcho(false).withEscapeSequence("/no").withTimeout(60).thatExcludesNonPlayersWithMessage("How did u get there???");
-                player.beginConversation(factory.buildConversation(player));
-            }
-        });
+        toReturn.put(21, new ConversationButton<>(
+                new ItemBuilder(Material.PAPER, "&dPrefix").lore(rank.getPrefix()).build(),
+                rank, "&aEnter the new prefix for this rank:",
+                (rank1, pair) -> {
+                    rank.setPrefix(pair.getB() + " ");
+                    rank.save();
+                    pair.getA().getForWhom().sendRawMessage(CC.translate("&aSuccessfully changed the prefix to " + pair.getB() ));
+                }
+        ));
+        toReturn.put(22, new ConversationButton<>(
+                new ItemBuilder(Material.PAPER, "&dDisplay Name").lore(rank.getDisplayName()).build(),
+                rank, "&aEnter the new display name for this rank:",
+                (rank1, pair) -> {
+                    rank.setDisplayName(pair.getB());
+                    rank.save();
+                    pair.getA().getForWhom().sendRawMessage(CC.translate("&aSuccessfully changed the displayname to " + pair.getB() ));
+                }
+        ));
+        toReturn.put(23, new ConversationButton<>(
+                new ItemBuilder(Material.PAPER, "&dSuffix").lore(rank.getSuffix()).build(),
+                rank, "&aEnter the new suffix for this rank:",
+                (rank1, pair) -> {
+                    rank.setDisplayName(pair.getB() + " ");
+                    rank.save();
+                    pair.getA().getForWhom().sendRawMessage(CC.translate("&aSuccessfully changed the suffix to " + pair.getB() ));
+                }
+        ));
 
         return toReturn;
     }
