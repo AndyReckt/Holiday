3097a0d896d1ced8557214ba516f7bd97f57b3c4
Andy <alex123aze@gmail.com>
2022-12-31 16:53:11 +0100

    Fixes paginated menus's page buttons

--- bukkit/src/main/java/me/andyreckt/holiday/bukkit/util/menu/pagination/PageButton.java
+++ bukkit/src/main/java/me/andyreckt/holiday/bukkit/util/menu/pagination/PageButton.java
@@ -7,6 +7,7 @@ import me.andyreckt.holiday.bukkit.util.item.InventoryUpdate;
 import me.andyreckt.holiday.bukkit.util.item.ItemBuilder;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.SkullType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
@@ -22,15 +23,12 @@ public class PageButton extends Button {
 
     @Override
     public ItemStack getButtonItem(final Player player) {
-        ItemBuilder item = (this.mod > 0) ? new ItemBuilder(Heads.NEXT_PAGE.build()) : new ItemBuilder(Heads.PREVIOUS_PAGE.build());
-        if (this.hasNext(player)) {
-            item.displayname(ChatColor.translateAlternateColorCodes('&', (this.mod > 0) ? "&8» &7Next Page" : "&8« &7Previous Page"));
-        } else {
-            item = new ItemBuilder(Material.AIR);
-        }
-        return item.build();
+        ItemBuilder builder = new ItemBuilder(Material.SKULL_ITEM)
+                .durability(SkullType.PLAYER.ordinal())
+                .texture(this.mod > 0 ? Heads.NEXT_PAGE.getBase() : Heads.PREVIOUS_PAGE.getBase())
+                .displayname(ChatColor.translateAlternateColorCodes('&', (this.mod > 0) ? "&8» &7Next Page" : "&8« &7Previous Page"));
+        return hasNext(player) ? builder.build() : builder.lore("&cNo more pages.").build();
     }
-
     @Override
     public void clicked(final Player player, final int i, final ClickType clickType, final int hb) {
         if (this.hasNext(player)) {
